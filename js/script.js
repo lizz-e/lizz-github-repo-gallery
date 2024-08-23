2e443cdf225261a51ec9caa52fc2ff84531246a4
lizzette_e <lizz.enorme@gmail.com>
2024-08-22 19:49:07 -0500

    Created functions to fetch repo data and to display the repos on the website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // .overview - div for profile info
 const profile = document.querySelector(".overview");
 const username = "lizz-e";
+const repoList = document.querySelector(".repo-list");
 
 //// fetch API JSON data ////
 // fetch github profile data
@@ -33,4 +34,33 @@ const displayProfile = function (data) {
     // add div to the profile (.overview element)
     profile.append(div);
     // add func() to the bottom of getProfile
+
+    getRepos();
+    // getRepos shows the repos that the user created
+};
+
+// fetch repo data //
+// fetch repos
+const getRepos = async function () {
+    // end point is repos. sort repos by most recent update is sort=update. show 100 repos per page is per_page=100
+    const grabRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
+    const repoData = await grabRepos.json();
+    // console.log(repoData);
+    displayRepos(repoData);
+}; 
+// placed getRepos() in displayProfile() to fetch & display repo
+
+// display repos
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        // create a list
+        const repoItem = document.createElement("li");
+        // create repo class
+        repoItem.classList.add("repo");
+        // show repo name on the website
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        // add the items to the repo list
+        repoList.append(repoItem);
+    };
+    // add displayRepos() at getRepos()
 };
