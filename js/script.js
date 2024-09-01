7fe3f23c4b0a21a3e8e58f689624d9a0974d240f
lizzette_e <lizz.enorme@gmail.com>
2024-09-01 18:18:12 -0500

    Created a back button on single repo page to lead to main page and created a dynamic search box

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const username = "lizz-e";
 const repoList = document.querySelector(".repo-list");
 const repoSection = document.querySelector(".repos");
 const repoDataSection = document.querySelector(".repo-data");
+const viewReposButton= document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 //// fetch API JSON data ////
 // fetch github profile data
@@ -37,13 +39,13 @@ const displayProfile = function (data) {
     profile.append(div);
     // add func() to the bottom of getProfile
 
-    getRepos();
+    getRepos(username);
     // getRepos shows the repos that the user created
 };
 
 // fetch repo data //
 // fetch repos
-const getRepos = async function () {
+const getRepos = async function (username) {
     // end point is repos. sort repos by most recent update is sort=update. show 100 repos per page is per_page=100
     const grabRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
     const repoData = await grabRepos.json();
@@ -54,6 +56,8 @@ const getRepos = async function () {
 
 // display repos
 const displayRepos = function (repos) {
+    // unhide the search bar on repos section
+    filterInput.classList.remove("hide");
     for (const repo of repos) {
         // create a list
         const repoItem = document.createElement("li");
@@ -80,7 +84,7 @@ repoList.addEventListener("click", function (e) {
 const getRepoInfo = async function (repoName) {
     const getInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
     const repoInfo = await getInfo.json();
-    console.log(repoInfo);
+    // console.log(repoInfo);
 
     // create array of languages
     // get languages
@@ -119,4 +123,43 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoDataSection.classList.remove("hide");
     // hide repo section
     repoSection.classList.add("hide");
+    viewReposButton.classList.remove("hide");
 };
+
+// add click event to the back button to the repo list page
+
+viewReposButton.addEventListener("click", function () {
+    // show the repos
+    repoSection.classList.remove("hide");
+    // hide the individual repo data
+    repoDataSection.classList.add("hide");
+    // hide back to repo gallery
+    viewReposButton.classList.add("hide");
+});
+
+// add input event to the search box on line 60 on displayRepos() func
+filterInput.addEventListener("input", function (e) {
+    // capture value of search text
+    const searchText= e.target.value;
+    // console.log = searchText;
+
+    // select all elements on the page with the class of repo
+    const repos = document.querySelectorAll(".repo");
+
+    // assign lowercase value of search text
+    const lowerCaseText = searchText.toLowerCase();
+
+    // loop through each repo inside of repos element.
+    for (const repo of repos) {
+        // lowercase the repo text 
+        const repoText = repo.innerText.toLowerCase();
+
+        // check if lowercase repo mathces the search text
+        if (repoText.includes(lowerCaseText)) {
+            repo.classList.remove("hide");
+        }else {
+            repo.classList.add("hide");
+        }
+        // if the repo contains the text show. if not hide the repo
+    };
+});
